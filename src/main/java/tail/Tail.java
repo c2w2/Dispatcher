bf09c5ac11fb1a7fb5c349d978dbda47f663940c
c2w2 <juchang0308@gmail.com>
2016-11-02 21:05:04 +0900

    Update Tail.java

--- src/main/java/tail/Tail.java
+++ src/main/java/tail/Tail.java
@@ -1,27 +1,16 @@
 package tail;
 
 
-//Import the Java classes
+
 import java.util.*;
 import java.io.*;
-/*
-import kafka.javaapi.producer.Producer; 
-import kafka.producer.KeyedMessage; 
-import kafka.producer.ProducerConfig; 
-/**
-* Implements console-based log file tailing, or more specifically, tail following:
-* it is somewhat equivalent to the unix command "tail -f"
-*/
+
 public class Tail implements LogFileTailerListener
 {
-/**b
-* The log file tailer
-*/
+
 private LogFileTailer tailer;
 
-/**
-* Creates a new Tail instance to follow the specified file
-*/
+
 public Tail( String filename , String num_t)
 {
  tailer = new LogFileTailer( new File( filename ), 1000, false , num_t);
@@ -29,23 +18,15 @@ public Tail( String filename , String num_t)
  tailer.run_1();
 }
 
-/**
-* A new line has been added to the tailed log file
-* 
-* @param line   The new line that has been added to the tailed log file
-*/
+
 public void newLogFileLine(String line)
 {
-// System.out.println( line );
-	
     	
     	
 	
 }
 
-/**
-* Command-line launcher
-*/
+
 public static void main( String[] args )
 {
  if( args.length < 1 )
